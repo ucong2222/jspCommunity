acaaf96396d0264e0fd22bc0e4cf2281bd5b16b3
ucong2222 <dbrudrjf21@gmail.com>
2021-01-07 10:53:57 +0900

    DB 스키마 추가

--- src/com/sbs/example/jspCommunity/servlet/usr/HomeGugudan2Servlet.java
+++ src/com/sbs/example/jspCommunity/servlet/usr/HomeGugudan2Servlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/usr/home/gugudan2")
-public class HomeGugudanServlet2 extends HttpServlet {
+public class HomeGugudan2Servlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
