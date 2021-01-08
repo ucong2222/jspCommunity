7ec82021035d9593e46e09d168964d46965d9288
ucong2222 <dbrudrjf21@gmail.com>
2021-01-08 17:09:22 +0900

    컨트롤러 구조 도입

--- src/com/sbs/example/jspCommunity/container/Container.java
+++ src/com/sbs/example/jspCommunity/container/Container.java
@@ -1,23 +1,30 @@
 package com.sbs.example.jspCommunity.container;
 
+import com.sbs.example.jspCommunity.controller.usr.ArticleController;
+import com.sbs.example.jspCommunity.controller.usr.MemberController;
 import com.sbs.example.jspCommunity.dao.ArticleDao;
 import com.sbs.example.jspCommunity.dao.MemberDao;
 import com.sbs.example.jspCommunity.service.ArticleService;
 import com.sbs.example.jspCommunity.service.MemberService;
 
 public class Container {
-		public static ArticleService articleService;
-		public static ArticleDao articleDao;
-		
-		public static MemberService memberService;
-		public static MemberDao memberDao;
+	public static ArticleService articleService;
+	public static ArticleDao articleDao;
 
-	
+	public static MemberService memberService;
+	public static MemberDao memberDao;
+
+	public static MemberController memberController;
+	public static ArticleController articleController;
+
 	static {
 		memberDao = new MemberDao();
 		articleDao = new ArticleDao();
-				
+
 		memberService = new MemberService();
 		articleService = new ArticleService();
+
+		memberController = new MemberController();
+		articleController = new ArticleController();
 	}
 }
