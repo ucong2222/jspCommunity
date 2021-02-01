0b41b6805bd92fa30bf8e02a9c9b01bc2209ef18
ucong2222 <dbrudrjf21@gmail.com>
2021-02-01 11:18:29 +0900

    attr 테이블 DB추가

--- db.sql
+++ db.sql
@@ -130,3 +130,28 @@ ALTER TABLE `member` CHANGE loginId loginId CHAR(50) NOT NULL AFTER updateDate,
 # 기존회원의 비번을 암호화
 UPDATE `member`
 SET loginPw = SHA2(loginPw, 256);
+
+# 부가정보 테이블
+# 댓글 테이블 추가
+
+CREATE TABLE attr(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `relTypeCode` CHAR(20) NOT NULL, # 관련타입코드, member
+    `relId` INT(10) UNSIGNED NOT NULL, # 관련데이터번호, 5
+    `typeCode` CHAR(30) NOT NULL, # extra
+    `type2Code` CHAR(30) NOT NULL, # isTempPasswordUsing
+    `value` TEXT NOT NULL # 1
+);
+
+# attr 유니크 인데스 걸기
+## 중복변수 생성금지
+## 변수찾는 속도 최적화
+ALTER TABLE `attr` ADD UNIQUE INDEX (`relTypeCode`, `relId`, `typeCode`,`type2Code`);
+
+## 특정 조건을 만족하는 회원 또는 게시물(기타 데이터)를 빠르게 찾기 위해서
+ALTER TABLE `attr` ADD INDEX (`relTypeCode`,`typeCode`,`type2Code`);
+
+# attr에 만료날짜 추가
+ALTER TABLE `attr` ADD COLUMN `expireDate` DATETIME NULL AFTER `value`;
